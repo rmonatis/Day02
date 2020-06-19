ec2fb02426dfe5338b250b18dddabdbfb5bbe002
Rorisang Monatisa <rmonatis@c4r2s5.wethinkcode.co.za>
2020-06-19 16:38:33 +0200

    exercise02

--- ex02/ft_print_number.c
+++ ex02/ft_print_number.c
@@ -6,22 +6,28 @@
 /*   By: rmonatis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 12:44:47 by rmonatis          #+#    #+#             */
-/*   Updated: 2020/06/19 13:22:13 by rmonatis         ###   ########.fr       */
+/*   Updated: 2020/06/19 16:37:46 by rmonatis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+
 #include <unistd.h>
 
-void		ft_putchar()
+int ft_putchar (char a)
+{
+	write(1, &a, 1);
+	return (0);
+}
+
+void ft_print_numbers(void)
 {
 	char a;
 	a = '0';
-	while( a <= '9' )
+	while(a <= '9')
 	{
-		write (1, &a, 1);
+		ft_putchar(a);
 		a = a + 1;
 	}
-	write (1, "\n", 1);
 }
 
