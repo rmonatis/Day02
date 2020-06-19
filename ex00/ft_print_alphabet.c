b40a82d58d2d8d3f8e9736c280550ef9c1f6f67c
Rorisang Monatisa <rmonatis@c4r2s5.wethinkcode.co.za>
2020-06-19 16:31:06 +0200

    exercise00

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,22 +6,27 @@
 /*   By: rmonatis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 10:04:35 by rmonatis          #+#    #+#             */
-/*   Updated: 2020/06/19 11:04:31 by rmonatis         ###   ########.fr       */
+/*   Updated: 2020/06/19 16:30:24 by rmonatis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <unistd.h>
 
-void		ft_putchar()
+int ft_putchar (char a)
+{
+	write(1, &a, 1);
+	return (0);
+}
+
+void ft_print_alphabet(void)
 {
 	char a;
 	a = 'a';
-	while( a <= 'z' )
+	while(a <= 'z')
 	{
-		write (1, &a, 1);;
+		ft_putchar(a);
 		a = a + 1;
 	}
-	write (1, "\n", 1);
 }
-
 
